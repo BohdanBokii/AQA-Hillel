70185bd2693fcc1e83768f1c19f3ccd08f5f201c
BohdanBokii <107073256+BohdanBokii@users.noreply.github.com>
2022-06-24 21:57:26 +0300

    Update BigDog.java

--- HomeWork6/src/BigDog.java
+++ HomeWork6/src/BigDog.java
@@ -3,15 +3,17 @@ public class BigDog extends Dog {
     public BigDog(String name) {
         super(name);
     }
+    
     @Override
     void greets() {
         System.out.println("Woow");
     }
+    
     @Override
     void greets(Dog another) {
         System.out.println("Woooow");
     }
-    @Override
+   
     void greets(BigDog another) {
         System.out.println("Woooooooow");
     }
