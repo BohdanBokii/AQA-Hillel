0c6e57c2b6c47178fc94931c2fe2cb414e8397bc
BohdanBokii <bohdanbwork95@gmail.com>
2022-07-06 22:17:23 +0300

    -- Create Method

---
+++ HomeWork8/src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        Method m = new Method();
+
+        String userInput = m.Input();
+        m.ProcessList(userInput);
+    }
+}
---
+++ HomeWork8/src/Method.java
@@ -0,0 +1,68 @@
+import java.util.*;
+
+public class Method {
+    public String Input() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Input sentence");
+        String str = scanner.nextLine();
+        return str;
+    }
+
+    public void ProcessList(String input) {
+        String[] words = ReplaceAndSplit(input);
+        words = GetUniqueNonEmptyWords(words);
+        FirstLetterToUppercase(words);
+
+        ShowList(words);
+    }
+
+    private String[] ReplaceAndSplit(String input) {
+         /*Replace all required symbols
+         Replace characters with one common*/
+        Character splitSymbol = '#';
+        Set<Character> symbols = new HashSet<Character>() {
+            {
+                add(',');
+                add('.');
+                add(' ');
+            }
+        };
+
+        for (Character symbol : symbols)
+            input = input.replace(symbol, splitSymbol);
+
+
+        /*Split string by words*/
+        String[] words = input.split(splitSymbol.toString());
+
+        return words;
+    }
+    /*Method returns unique values with case sensitivity*/
+    private String[] GetUniqueNonEmptyWords(String[] words) {
+        Set<String> uniqueWords = new TreeSet<>(String.CASE_INSENSITIVE_ORDER);
+
+        for (String word : words) {
+            if (word.isEmpty())
+                continue;
+
+            uniqueWords.add(word);
+        }
+        /*List to array*/
+        return uniqueWords.toArray(new String[uniqueWords.size()]);
+    }
+
+    private void FirstLetterToUppercase(String[] words) {
+        for (int i = 0; i < words.length; i++) {
+            String firstLetter = words[i].substring(0, 1);
+            words[i] = words[i]
+                    .toLowerCase()
+                    .replaceFirst(firstLetter, firstLetter.toUpperCase());
+        }
+    }
+
+    private void ShowList(String[] words) {
+        for (String word : words) {
+            System.out.println(word);
+        }
+    }
+}
