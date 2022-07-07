596286871ee8b5b22255bfac54f6585530acb3ae
Moronbenz <107073256+BohdanBokii@users.noreply.github.com>
2022-07-07 19:16:48 +0300

    Update Method.java
    Comment

--- HomeWork8/src/Method.java
+++ HomeWork8/src/Method.java
@@ -51,7 +51,7 @@ public class Method {
         /*List to array*/
         return uniqueWords.toArray(new String[uniqueWords.size()]);
     }
-        /*Iterates over letters, lowercase, uppercase the first letter of each word*/
+        /*Iterates over words,transform lowercase,change the capitalization of first letter of each word*/
     private void FirstLetterToUppercase(String[] words) {
         for (int i = 0; i < words.length; i++) {
             String firstLetter = words[i].substring(0, 1);
