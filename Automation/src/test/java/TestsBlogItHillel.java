c878a66aa10327216b2ce39465c2b83464f4f1cb
BohdanBokii <bohdanbwork95@gmail.com>
2022-08-06 00:22:24 +0300

    -- Parameterized test with several parameters

--- Automation/src/test/java/TestsBlogItHillel.java
+++ Automation/src/test/java/TestsBlogItHillel.java
@@ -12,6 +12,56 @@ import java.util.Collection;
 
 @RunWith(Parameterized.class)
 public class TestsBlogItHillel extends Base {
+    private Integer expectedCountOfNews;
+    private String topic;
+
+    private static final String BASEURL = "https://ithillel.ua/";
+    private static BlogItHillel Blog;
+    private static BlogPage blogPage;
+
+    public TestsBlogItHillel(String topic, Integer expectedCountOfNews) {
+        this.topic = topic;
+        this.expectedCountOfNews = expectedCountOfNews;
+    }
+
+    @Parameterized.BeforeParam
+    public static void beforeParam() {
+        driver.get(BASEURL);
+
+        Blog = new BlogItHillel(driver);
+        Blog.clickBlogButton();
+
+        blogPage = new BlogPage(driver);
+    }
+    //Parameterized test with several parameters
+    @Parameterized.Parameters(name = "Topic - {0} count news: {1}")
+    public static Collection news() {
+        return Arrays.asList(new Object[][]{
+                {"frontend", 114},
+                {"qa", 83},
+                {"management", 96},
+                {"marketing", 100},
+        });
+    }
+
+    @Test
+    public void testBlogNews() {
+        // Arrange
+
+        // Act
+        blogPage.clickBlogMenu(topic);
+        Util.scrollToElementVisibilityOf(driver, By.cssSelector("div.posts_spinner"));
+
+        // Assert
+        int count = blogPage.getAmountListNews();
+        System.out.println(topic + " news: " + count);
+        Assert.assertNotEquals(0, count);
+        Assert.assertEquals((long)expectedCountOfNews, count);
+    }
+}
+
+/*@RunWith(Parameterized.class)
+public class TestsBlogItHillel extends Base {
     private static final String BASEURL = "https://ithillel.ua/";
     private static BlogItHillel Blog;
     private static BlogPage blogPage;
@@ -52,5 +102,5 @@ public class TestsBlogItHillel extends Base {
             System.out.println(topic + " news: " + count);
             Assert.assertNotEquals(0, count);
         }
-    }
+    }*/
 
